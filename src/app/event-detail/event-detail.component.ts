85714a646a8c4519032f31ba1c8726eb8d71bfb7
Rajiniks <rajiniks1994@gmail.com>
2018-10-19 17:53:37 +0530

    commit

--- src/app/event-detail/event-detail.component.ts
+++ src/app/event-detail/event-detail.component.ts
@@ -1,64 +1,9 @@
-import { Component,  } from '@angular/core';
-import { MouseEvent } from '@agm/core';
+import { Component } from "@angular/core";
+import { MouseEvent } from "@agm/core";
 
 @Component({
-  selector: 'app-event-detail',
-  templateUrl: './event-detail.component.html',
-  styleUrls: ['./event-detail.component.css']
+  selector: "app-event-detail",
+  templateUrl: "./event-detail.component.html",
+  styleUrls: ["./event-detail.component.css"]
 })
-export class EventDetailComponent {
-// google maps zoom level
-zoom = 10;
-
-// initial center position for the map
-lat = 19.0760;
-lng = 72.8777;
-
-clickedMarker(label: string, index: number) {
-  console.log(`clicked the marker: ${label || index}`);
-}
-
-mapClicked($event: MouseEvent) {
-  this.markers.push({
-    lat: $event.coords.lat,
-    lng: $event.coords.lng,
-    draggable: true
-  });
-}
-
-markerDragEnd(m: marker, $event: MouseEvent) {
-  console.log('dragEnd', m, $event);
-}
-
-markers: marker[] = [
-  {
-    lat: 19.1591362,
-    lng: 72.9596153,
-    label: 'A',
-    draggable: true
-  },
-  {
-    lat: 19.259100,
-    lng: 72.9596100,
-    label: 'B',
-    draggable: false
-  },
-  {
-    lat: 19.1591400,
-    lng: 72.9996200,
-    label: 'C',
-    draggable: true
-  }
-];
-}
-
-// just an interface for type safety.
-interface marker {
-lat: number;
-lng: number;
-label?: string;
-draggable: boolean;
-}
-
-
-
+export class EventDetailComponent {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Router, RouterModule, Routes } from '@angular/router';
-import { EventService } from 'src/app/event.service';
+import { Component, OnInit, Input } from "@angular/core";
+import { Router, RouterModule, Routes } from "@angular/router";
+import { EventService } from "src/app/event.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent {
   events: any;
-  constructor(private eventService: EventService, private router: Router) {}
+  constructor(private eventService: EventService) {}
 
   // tslint:disable-next-line:use-life-cycle-interface
   ngOnInit() {
